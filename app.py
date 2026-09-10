6c2edaa0ae6be81bdf3c40a8b5dddc204d488c1b
rehandbit <rehan.farazmdp@gmail.com>
2026-09-11 01:15:34 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+from src.pipeline.prediction_pipeline import PredictionPipeline, CustomData
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', method = ['POST'])
+def predict():
+    data = CustomData(
+        symboling         = int(request.form.get("symboling")),
+        normalized_losses = int(request.form.get("normalized_losses")),
+        make              = request.form.get("make"),
+        fuel_type         = request.form.get("fuel_type"),
+        aspiration        = request.form.get("aspiration"),
+        num_of_doors      = int(request.form.get("num_of_doors")),
+        body_style        = request.form.get("body_style"),
+        drive_wheels      = request.form.get("drive_wheels"),
+        engine_location   = request.form.get("engine_location"),
+        wheel_base        = float(request.form.get("wheel_base")),
+        length            = float(request.form.get("length")),
+        width             = float(request.form.get("width")),
+        height            = float(request.form.get("height")),
+        curb_weight       = int(request.form.get("curb_weight")),
+        engine_type       = request.form.get("engine_type"),
+        num_of_cylinders  = int(request.form.get("num_of_cylinders")),
+        engine_size       = int(request.form.get("engine_size")),
+        fuel_system       = request.form.get("fuel_system"),
+        bore              = float(request.form.get("bore")),
+        stroke            = float(request.form.get("stroke")),
+        compression_ratio = float(request.form.get("compression_ratio")),
+        horsepower        = int(request.form.get("horsepower")),
+        peak_rpm          = int(request.form.get("peak_rpm")),
+        city_mpg          = int(request.form.get("city_mpg")),
+        highway_mpg       = int(request.form.get("highway_mpg")),
+    )
+
+    df = data.get_data_as_dataframe()
+    pipeline = PredictionPipeline()
+    price = pipeline.predict(df)
+
+    return render_template('index.html', prediction = f'Predicted Price: $ {price[0]:.2f}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ src/components/model_evaluation.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+import pandas as pd
+
+from src.logger import logger
+from src.exception import CustomException
+from src.utils import load_object
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+
+class ModelEvaluation:
+    def initiate_model_evaluation(self,x_test, y_test):
+        try:
+            logger.info('Model evaluation started')
+
+            model = load_object('artifacts/model/model.pkl')
+
+            y_pred = model.predict(x_test)
+            r2 = r2_score(y_test, y_pred)
+            rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+            mae = mean_absolute_error(y_test, y_pred)
+
+            result_df = pd.DataFrame({
+                'Actual Price ' : y_test.values,
+                'Predicted Price' : y_pred.round(0),
+                'Difference'      : ((y_test.values- y_pred).round(0)),
+                'Difference %'    : ((abs(y_test.values - y_pred) / y_test.values)*100).round(1)
+            })
+
+            print('\n ---Model Evaluation ---')
+            print(f'R2 : {r2:.2f}')
+            print(f'RMSE : {rmse:.2f}')
+            print(f'MAE : {mae:.2f}')
+
+            print(result_df.to_string())
+
+            within_10 = (result_df['Difference %']<=10).sum()
+            total = len(result_df)
+            print(f'\n {within_10}/{total} Prediction within 10 error')
+            print(f'Accuracy within 10% {within_10/total*100:.1f}%')
+
+            logger.info(f"R2: {r2}  RMSE: {rmse}  MAE: {mae}")
+            logger.info(f"Accuracy within 10%: {round(within_10/total*100, 1)}%")
+
+            return r2, rmse, mae
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__ == '__main__':
+    from src.components.data_ingestion import DataIngestion
+    from src.components.data_transformation import DataTransformation
+
+    ingestion = DataIngestion()
+    train_path, test_path = ingestion.initiate_data_ingestion()
+
+    transformation = DataTransformation()
+    x_train, x_test, y_train , y_test = transformation.initiate_data_transformation(train_path, test_path)
+
+    evaluation =ModelEvaluation()
+    evaluation.initiate_model_evaluation(x_test, y_test)
